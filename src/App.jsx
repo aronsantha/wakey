205f2f1e5719b00734032ec1c4ff1ca9ff2b5689
aronsantha <108655791+aronsantha@users.noreply.github.com>
2025-10-01 22:00:44 +0200

    clean up boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [offset, setOffset] = useState(15);
+  const now = new Date();
+  const options = { hour: "numeric", minute: "numeric" };
+
+  const step = 60 * 60 * 1000;
+  const sevenAndAHalfHours = new Date(now.getTime() + step * 7.5);
+  const nineHours = new Date(now.getTime() + step * 9);
 
   return (
     <>
+      <h1>{now.toLocaleTimeString("en-US", options)}</h1>
+      <h2>{sevenAndAHalfHours.toLocaleTimeString("en-US", options)}</h2>
+      <h2>{nineHours.toLocaleTimeString("en-US", options)}</h2>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => setOffset((offset) => offset + 15)}>offset is {offset}</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
