0a2105529fdc7b0d1863ef88c570adebd768489b
aronsantha <108655791+aronsantha@users.noreply.github.com>
2025-10-02 00:53:22 +0200

    save preferences to localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
+import { useStickyState } from "./hooks.js";
 import "./App.css";
+import BatterySvg from "./assets/battery.svg";
 
 function App() {
-  const [offsetInMins, setOffset] = useState(15);
+  const [offsetInMins, setOffset] = useStickyState(15, "time-to-fall-asleep");
+  const [timeFormat, setTimeFormat] = useStickyState("12h", "time-format");
   const offsetInMs = offsetInMins * 60 * 1000;
   const [time, setTime] = useState(new Date());
 
@@ -19,13 +22,28 @@ function App() {
   const step = 60 * 60 * 1000;
   const sevenAndAHalfHours = new Date(time.getTime() + offsetInMs + step * 7.5);
   const nineHours = new Date(time.getTime() + offsetInMs + step * 9);
+
+  // TODO: add ability to switch between 12h and 24h format, save in localstorage
+  // TODO: add ability to set time it takes to fall asleep, save in localstorage
   function formatTime(date) {
-    return date.toLocaleTimeString("en-US", { hour: "numeric", minute: "numeric" });
+    return date.toLocaleTimeString(timeFormat === "24h" ? "en-GB" : "en-US", {
+      hour: "numeric",
+      minute: "numeric",
+    });
   }
+  const toggleTimeFormat = () => {
+    setTimeFormat((prevFormat) => (prevFormat === "12h" ? "24h" : "12h"));
+  };
 
   return (
     <>
       <h1>Current time: {formatTime(time)}</h1>
+      <img
+        src={BatterySvg}
+        alt="Battery"
+        style={{ background: "red", height: "100px", width: "120px" }}
+      />
+
       <h2>Recommended wakeup times:</h2>
       <p>{formatTime(sevenAndAHalfHours)}</p>
       <p>{formatTime(nineHours)}</p>
@@ -33,6 +51,7 @@ function App() {
         <button onClick={() => setOffset((offsetInMins) => offsetInMins + 15)}>
           Time to fall asleep is {offsetInMins} minutes
         </button>
+        <button onClick={toggleTimeFormat}>Currently using {timeFormat} clock.</button>
       </div>
     </>
   );
