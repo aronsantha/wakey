e624e606ff66ffb1119d1adb55bb7470477f8b0a
aronsantha <108655791+aronsantha@users.noreply.github.com>
2025-10-01 22:34:45 +0200

    update clock every second

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [offsetInMins, setOffset] = useState(15);
   const offsetInMs = offsetInMins * 60 * 1000;
-  const now = new Date();
-  const options = { hour: "numeric", minute: "numeric" };
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      window.clearInterval(intervalId);
+    };
+  }, []);
 
   const step = 60 * 60 * 1000;
-  const sevenAndAHalfHours = new Date(now.getTime() + offsetInMs + step * 7.5);
-  const nineHours = new Date(now.getTime() + offsetInMs + step * 9);
+  const sevenAndAHalfHours = new Date(time.getTime() + offsetInMs + step * 7.5);
+  const nineHours = new Date(time.getTime() + offsetInMs + step * 9);
+  function formatTime(date) {
+    return date.toLocaleTimeString("en-US", { hour: "numeric", minute: "numeric" });
+  }
 
   return (
     <>
-      <h1>Current time: {now.toLocaleTimeString("en-US", options)}</h1>
+      <h1>Current time: {formatTime(time)}</h1>
       <h2>Recommended wakeup times:</h2>
-      <p>{sevenAndAHalfHours.toLocaleTimeString("en-US", options)}</p>
-      <p>{nineHours.toLocaleTimeString("en-US", options)}</p>
+      <p>{formatTime(sevenAndAHalfHours)}</p>
+      <p>{formatTime(nineHours)}</p>
       <div>
         <button onClick={() => setOffset((offsetInMins) => offsetInMins + 15)}>
           Time to fall asleep is {offsetInMins} minutes
