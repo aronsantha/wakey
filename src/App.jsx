9d824c3839bc0f7fee8b62e7f72bd888abf6fa81
aronsantha <108655791+aronsantha@users.noreply.github.com>
2025-10-02 14:47:10 +0200

    ui improvements wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useStickyState } from "./hooks.js";
 import RadialBar from "./RadialBar.jsx";
-import "./App.css";
 
 function App() {
   const [offsetInMins, setOffset] = useStickyState(15, "time-to-fall-asleep");
@@ -40,33 +39,49 @@ function App() {
 
   return (
     <>
-      <h1 className="pacifico">Time to sleep...</h1>
-      <div className="flex justify-between flex-col gap-6 my-10 w-full">
-        <h2>Recommended wakeup times:</h2>
-
-        {sleepLengthArray
-          .map((interval, index) => (
-            <div
-              key={index}
-              className="bg-[#4d4756] py-2 px-10 rounded-2xl  flex items-center justify-between w-full "
-            >
-              <p className="text-3xl font-bold">{formatTime(interval)}</p>
-              <div className="h-20 shrink-0">
-                <RadialBar intervalHours={intervals[index]} ranking={index} />
-              </div>
-            </div>
-          ))
-          .reverse()}
-      </div>
-
-      <div className="flex flex-col gap-4">
-        <div>
-          <button onClick={() => setOffset((offsetInMins) => offsetInMins + 15)}>
-            If you fall asleep in {offsetInMins} mins.
-          </button>
-          <button onClick={() => setOffset(15)}>↩️</button>
+      <div className="stars"></div>
+      <svg xmlns="http://www.w3.org/2000/svg" className="noise">
+        <filter id="noise" x="0" y="0">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.65"
+            numOctaves="3"
+            stitchTiles="stitch"
+          />
+          <feBlend mode="screen" />
+        </filter>
+        <rect width="100%" height="100%" filter="url(#noise)" opacity="0.5" />
+      </svg>
+      <div className=" w-full max-w-[1200px] mx-auto text-center">
+        <h1 className="pacifico">Time to sleep...</h1>
+        <div className="flex justify-between flex-col items-center gap-6 my-10 w-full px-2">
+          <h2>Recommended wakeup times:</h2>
+          <div className="overflow-auto h-[500px] flex flex-col gap-3 w-full  max-w-[800px] no-scrollbar">
+            {sleepLengthArray
+              .map((interval, index) => (
+                <div
+                  key={index}
+                  className="bg-black/40 backdrop-blur-sm py-5 px-10 rounded-2xl flex justify-between items-center gap-4 "
+                >
+                  <p className="text-5xl  barlow ">{formatTime(interval)}</p>
+                  <div className="h-20 shrink-0">
+                    <RadialBar intervalHours={intervals[index]} ranking={index} />
+                  </div>
+                </div>
+              ))
+              .reverse()}
+          </div>
         </div>
-        <button onClick={toggleTimeFormat}>Currently using {timeFormat} clock.</button>
+
+        <div className="flex flex-col gap-4">
+          <div>
+            <button onClick={() => setOffset((offsetInMins) => offsetInMins + 15)}>
+              If you fall asleep in {offsetInMins} mins.
+            </button>
+            <button onClick={() => setOffset(15)}>↩️</button>
+          </div>
+          <button onClick={toggleTimeFormat}>Currently using {timeFormat} clock.</button>
+        </div>
       </div>
     </>
   );
--- src/RadialBar.jsx
+++ src/RadialBar.jsx
@@ -9,7 +9,7 @@ const RadialBar = ({ intervalHours, ranking }) => {
   const dashOffset = circumference * (1 - percentage / 100);
 
   // Adjust text ranking slightly based on number of digits
-  const textX = intervalHours && (intervalHours.toString().length === 3 ? "58px" : "75px");
+  const textX = intervalHours && (intervalHours.toString().length === 3 ? "65px" : "80px");
 
   return (
     <svg
@@ -19,15 +19,15 @@ const RadialBar = ({ intervalHours, ranking }) => {
       xmlns="http://www.w3.org/2000/svg"
       style={{ transform: "rotate(-90deg)" }}
     >
-      <circle r="90" cx="100" cy="100" fill="transparent" stroke="#55505b" strokeWidth="20" />
+      <circle r="90" cx="100" cy="100" fill="transparent" stroke="#FFFFFF09" strokeWidth="5" />
 
       <circle
         r="90"
         cx="100"
         cy="100"
         fill="transparent"
-        stroke="#509e86"
-        strokeWidth="20"
+        stroke="#FEC119"
+        strokeWidth="10"
         strokeLinecap="round"
         strokeDasharray={`${circumference}px`}
         strokeDashoffset={`${dashOffset}px`}
@@ -36,10 +36,9 @@ const RadialBar = ({ intervalHours, ranking }) => {
       <text
         x={textX}
         y="115px"
-        fill="#e0e0e0"
-        fontSize="45px"
-        fontWeight="bold"
+        fontSize="50px"
         style={{ transform: "rotate(90deg) translate(0px, -196px)" }}
+        className="barlow font-light fill-white/60"
       >
         {intervalHours}h
       </text>
