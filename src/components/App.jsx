bdda895fafd5782ecb1bd3de4a302d06d3a2e7f1
aronsantha <108655791+aronsantha@users.noreply.github.com>
2025-10-03 12:54:54 +0200

    fix scroll issue

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useStickyState } from "../hooks.js";
-import RadialBar from "./RadialBar.jsx";
 import BaseModal from "./BaseModal.jsx";
 import { Cog6ToothIcon } from "@heroicons/react/20/solid";
 
@@ -37,47 +36,47 @@ function App() {
   return (
     <>
       <div className="stars"></div>
-
-      <div className="w-full max-w-[1200px] mx-auto text-center h-[100dvh]">
-        <Cog6ToothIcon
-          as={"button"}
-          tabindex="0"
-          onClick={() => setIsModalOpen(true)}
-          className="fixed top-4 right-4 -ml-5 cursor-pointer rounded-full text-white opacity-50 transition-all duration-75 hover:opacity-100 h-8"
-          aria-label="Open settings modal"
+      <Cog6ToothIcon
+        as={"button"}
+        tabIndex="0"
+        onClick={() => setIsModalOpen(true)}
+        className="fixed  top-4 right-4 -ml-5 cursor-pointer rounded-full text-white opacity-50 transition-all duration-75 hover:opacity-100 h-8"
+        aria-label="Open settings modal"
+      />
+      <div className="flex flex-col gap-4 z-50 absolute">
+        <BaseModal
+          type={"info"}
+          isOpen={isModalOpen}
+          timeFormat={timeFormat}
+          offsetInMins={offsetInMins}
+          handleClose={() => setIsModalOpen(false)}
+          handleSelectTimeFormat={(format) => () => setTimeFormat(format)}
+          handleSelectOffset={(mins) => setOffset(mins)}
         />
-        <h1 className="text-7xl caprasimo text-[#fec119] text-shadow-[0_0_40px_#fec119]">
-          It's sleepy time...
-        </h1>
-        <div className="flex justify-between flex-col items-center gap-6 my-10 w-full px-2">
-          <h2 className="text-[#e4af1d] font-serif text-2xl text-shadow-2xl">
-            If you fall asleep in <span className="font-bold">{offsetInMins} minutes</span>, here
-            are your ideal wake-up times for perfect, restful sleep.
-          </h2>
-          <p>Note: you can adjust your fall asleep time in the settings.</p>
-          <div className="flex-wrap flex overflow-y-auto gap-3 w-full max-w-[800px] max-h-[50vh] no-scrollbar px-5 py-5">
-            {sleepLengthArray.map((interval, index) => (
-              <div
-                key={index}
-                className="grow first:mb-10 first:shadow-[0_0_10px_#fec119] first:text-6xl first:py-18 first:w-full gap-5 text-xl bg-black/20 backdrop-blur-sm py-4  px-8    rounded-2xl  flex flex-col  "
-              >
-                <p className="barlow text-amber-200">{formatTime(interval)}</p>
-                <p className="text-xs opacity-40">{intervals[index]} hours</p>
-              </div>
-            ))}
+      </div>
+      <div className="absolute inset-0 flex flex-col gap-5 pt-20">
+        <div className="flex flex-col gap-8 px-5">
+          <h1 className="text-7xl caprasimo text-[#fec119] text-shadow-[0_0_40px_#fec119]">
+            It's sleepy time...
+          </h1>
+          <div className="flex flex-col gap-3 ">
+            <h2 className="text-[#e4af1d] font-serif text-xl text-shadow-2xl">
+              If you fall asleep in <span className="font-bold">{offsetInMins} minutes</span>, here
+              are your ideal wake-up times for perfect, restful sleep.
+            </h2>
+            <p className="text-xs">Note: you can adjust your fall asleep time in the settings.</p>
           </div>
         </div>
-
-        <div className="flex flex-col gap-4 z-50 absolute">
-          <BaseModal
-            type={"info"}
-            isOpen={isModalOpen}
-            timeFormat={timeFormat}
-            offsetInMins={offsetInMins}
-            handleClose={() => setIsModalOpen(false)}
-            handleSelectTimeFormat={(format) => () => setTimeFormat(format)}
-            handleSelectOffset={(mins) => setOffset(mins)}
-          />
+        <div className="flex-wrap flex overflow-y-scroll gap-3 w-full max-w-[800px] px-5 py-5">
+          {sleepLengthArray.map((interval, index) => (
+            <div
+              key={index}
+              className="grow items-center first:mb-3 first:shadow-[0_0_10px_#fec119] first:text-5xl first:py-8 first:w-full gap-5 text-xl bg-black/20 backdrop-blur-sm py-4 px-8 rounded-2xl flex flex-col"
+            >
+              <p className="barlow text-amber-200">{formatTime(interval)}</p>
+              <p className="text-xs opacity-40">{intervals[index]} hours</p>
+            </div>
+          ))}
         </div>
       </div>
     </>
