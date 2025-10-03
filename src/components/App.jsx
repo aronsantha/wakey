adb08d35e30638a8a82b52d582c434b509299ae6
aronsantha <108655791+aronsantha@users.noreply.github.com>
2025-10-03 22:13:24 +0200

    wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,31 +47,29 @@ function App() {
           handleSelectOffset={(mins) => setOffset(mins)}
         />
       </div> */}
-      <div className="absolute inset-0 h-dvh overflow-y-auto ">
-        <div className="flex flex-col gap-5 items-center justify-center">
-          <div className="flex flex-col  px-5">
-            <h1 className="text-4xl caprasimo text-[#fec119] py-14 text-shadow-[0_0_40px_#fec119]">
-              It's sleepy time...
-            </h1>
-            <div className="flex flex-col gap-3 ">
-              <h2 className="text-[#e4af1d] font-serif text-xl text-shadow-2xl">
-                If you fall asleep in <span className="font-bold">{offsetInMins} minutes</span>,
-                here are your ideal wake-up times for perfect, restful sleep.
-              </h2>
-              <p className="text-xs">Note: you can adjust your fall asleep time in the settings.</p>
+      <div className="absolute  overflow-y-auto inset-0 h-dvh flex flex-col gap-5 items-center justify-center">
+        <div className="flex flex-col px-5 pt-20 ">
+          <h1 className="text-4xl caprasimo text-[#fec119] py-5 text-shadow-[0_0_40px_#fec119]">
+            It's sleepy time...
+          </h1>
+          <div className="flex flex-col gap-3 ">
+            <h2 className="text-[#e4af1d] font-serif text-xl text-shadow-2xl">
+              If you fall asleep in <span className="font-bold">{offsetInMins} minutes</span>, here
+              are your ideal wake-up times for perfect, restful sleep.
+            </h2>
+            <p className="text-xs">Note: you can adjust your fall asleep time in the settings.</p>
+          </div>
+        </div>
+        <div className="flex-wrap flex  gap-3 w-full max-w-[800px] px-5 pt-5">
+          {sleepLengthArray.map((interval, index) => (
+            <div
+              key={index}
+              className="grow items-center first:mb-3 first:shadow-[0_0_10px_#fec119] first:text-5xl first:py-8 first:w-full gap-5 text-xl bg-black/20 backdrop-blur-sm py-4 px-8 rounded-2xl flex flex-col"
+            >
+              <p className="barlow text-amber-200">{formatTime(interval)}</p>
+              <p className="text-xs opacity-40">{intervals[index]} hours</p>
             </div>
-          </div>
-          <div className="flex-wrap flex  gap-3 w-full max-w-[800px] px-5 pt-5">
-            {sleepLengthArray.map((interval, index) => (
-              <div
-                key={index}
-                className="grow items-center first:mb-3 first:shadow-[0_0_10px_#fec119] first:text-5xl first:py-8 first:w-full gap-5 text-xl bg-black/20 backdrop-blur-sm py-4 px-8 rounded-2xl flex flex-col"
-              >
-                <p className="barlow text-amber-200">{formatTime(interval)}</p>
-                <p className="text-xs opacity-40">{intervals[index]} hours</p>
-              </div>
-            ))}
-          </div>
+          ))}
         </div>
       </div>
       {/* <div className="flex px-3 h-12 items-center backdrop-blur-lg bg-neutral-900/50 justify-end fixed bottom-0 w-full">
