15c454b329b26c7e24f0070df324fa42d9bbf28b
aronsantha <108655791+aronsantha@users.noreply.github.com>
2026-03-30 17:57:43 +0200

    rewrite scroll WIP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -219,7 +219,7 @@ function App() {
         handleClose={() => setShownModal("")}
         children={shownModal && modalMap[shownModal].content}
       />
-      <div className="flex flex-col items-center overflow-y-auto bg-emerald-700 md:justify-center">
+      <div className="fixed top-0 left-0 flex h-[100dvh] w-full flex-col items-center overflow-hidden bg-emerald-700 md:justify-center">
         {/* <footer
           role="tablist"
           className="p-safe fixed right-0 bottom-0 left-0 z-50 h-14 w-full border-t-[1px] border-neutral-100/20 bg-neutral-950/50 px-3 backdrop-blur-xl"
@@ -241,7 +241,7 @@ function App() {
             })}
           </div>
         </footer> */}
-        <main className="mt-16 mb-24 flex max-w-[500px] flex-col px-6 text-center">
+        <main className="flex h-full w-full max-w-[500px] flex-col overflow-auto px-6 text-center">
           <div className="mb-14 flex flex-col gap-4">
             <h1 className="font-caprasimo mb-8 text-4xl text-amber-500/80">
               It's sleepy time...
