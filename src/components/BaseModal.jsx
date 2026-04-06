ac3508197b0e3c7331680ac4e2558f61c5a6ca0f
aronsantha <108655791+aronsantha@users.noreply.github.com>
2026-04-06 15:15:49 +0200

    refactor slide-up: animations, transitions, media queries

--- src/components/BaseModal.jsx
+++ src/components/BaseModal.jsx
@@ -15,29 +15,30 @@ function BaseModal({ isOpen, handleClose, modalTitle, children }) {
       className="relative z-50 text-neutral-400 focus:outline-none"
       onClose={handleClose}
     >
-      <DialogBackdrop className="fixed inset-0 bottom-0 cursor-pointer overflow-hidden bg-black/20 backdrop-blur-[3px]" />
-      <div className="fixed inset-0 bottom-0 w-screen overflow-y-auto">
-        <div className="flex h-full items-end justify-center overflow-hidden sm:items-center">
-          <DialogPanel
-            transition
-            className="ease-apple max-h-[80dvh] w-full max-w-[640px] rounded-t-3xl border-t-[1px] border-neutral-100/15 bg-black/80 pb-7 shadow-lg backdrop-blur-xs transition-all duration-300 not-sm:pb-14 not-sm:data-[closed]:translate-y-full sm:rounded-3xl sm:border-[1px] sm:data-[closed]:opacity-0"
-          >
-            <div className="flex items-center justify-between p-4">
-              <DialogTitle className="font-heading grow text-center text-2xl">
-                {modalTitle}
-              </DialogTitle>
-              <CloseButton
-                as={"button"}
-                aria-label="Close modal"
-                className="-ml-9 w-9 cursor-pointer rounded-full border-[1px] border-neutral-100/30 p-1.5 text-neutral-100/50 transition-all duration-75 hover:text-neutral-100"
-              >
-                <XMarkIcon />
-              </CloseButton>
-            </div>
+      <DialogBackdrop
+        transition
+        className="animate-fade-in fixed inset-0 cursor-pointer overflow-hidden bg-black/30 sm:backdrop-blur-[3px]"
+      />
+      <div className="fixed inset-0 flex w-screen flex-col items-center justify-end overflow-hidden sm:justify-center">
+        <DialogPanel
+          transition
+          className="slide-panel w-full max-w-[640px] overflow-hidden rounded-t-3xl border-t-[1px] border-neutral-100/15 bg-black/80 pb-7 shadow-lg backdrop-blur-xs transition-all delay-80 not-sm:pb-14 not-sm:data-[closed]:translate-y-full sm:rounded-3xl sm:border-[1px] sm:data-[closed]:scale-0"
+        >
+          <div className="sticky top-0 flex items-center justify-between bg-gradient-to-b from-black from-70% to-transparent p-4">
+            <DialogTitle className="font-heading grow text-center text-2xl">
+              {modalTitle}
+            </DialogTitle>
+            <CloseButton
+              as={"button"}
+              aria-label="Close modal"
+              className="-ml-9 w-9 cursor-pointer rounded-full border-[1px] border-neutral-100/30 p-1.5 text-neutral-100/50 transition-all duration-75 hover:text-neutral-100"
+            >
+              <XMarkIcon />
+            </CloseButton>
+          </div>
 
-            <div className="pb-safe h-full overflow-y-auto">{children}</div>
-          </DialogPanel>
-        </div>
+          <div className="pb-safe h-full overflow-y-auto">{children}</div>
+        </DialogPanel>
       </div>
     </Dialog>
   );
